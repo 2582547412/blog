c5e9d44d12fb6d72514c56e49d7e91fe5409c91d
xiashisong <2582547412@qq.com>
2022-12-30 14:24:41 +0800

    修改备案号

--- ruoyi-ui/src/views/cms/Footer.vue
+++ ruoyi-ui/src/views/cms/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="footer">
-      <span style="color: #FFFFFF;line-height:40px;font-size: 12px;">Copyright © 2021-2025 Ning All Rights Reserved.</span>
+      <a href="https://beian.miit.gov.cn"><span style="color: #FFFFFF;line-height:40px;font-size: 12px;">皖ICP备2022017322号-1</span></a>
     </div>
 </template>
 
